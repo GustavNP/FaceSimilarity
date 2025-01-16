4b00db91e99e82d2eb772e6a3779cc06d2bc995a
Gustav <gustavnp@gmail.com>
2025-01-16 22:51:32 +0100

    Some fixes to make the code work specifically for the current naming and way other scripts work

--- combine_dissimilarity_and_quality_scores.py
+++ combine_dissimilarity_and_quality_scores.py
@@ -2,7 +2,8 @@ import os
 import csv
 
 # Initialize an empty list to store all lines
-similarity_file = "./similarity_scores_Facenet512_Yunet_LFW.csv"
+# similarity_file = "./similarity_scores_Facenet512_Yunet_LFW.csv"
+similarity_file = "./similarity_score_files/similarity_scores_Facenet512_yunet_VGGFace-200k-train-431-502.csv"
 
 similarity_scores = []
 with open(similarity_file) as fileObject:
@@ -24,7 +25,9 @@ print(len(similarity_scores))
 # quality_file = "./Flickr45k-LFW_trained_Predicted_UQS_LFW.csv"
 # quality_file = "./Flickr45k-LFW-trained_Scalar_Predicted-UQS_LFW.csv"
 # quality_file = "./LFW-trained_Scalar_Predicted-UQS_LFW.csv"
-quality_file = "./LFW-trained_Scalar-extra-removed_Predicted-UQS_LFW.csv"
+# quality_file = "./LFW-trained_Scalar-extra-removed_Predicted-UQS_LFW.csv"
+# quality_file = "./quality_score_files/VGGFace2-200k-all.csv"
+quality_file = "./quality_score_files/Predicted-UQS_Specific_Dataset_Scalar_to_Scalar_VGGFace200k-n431-502.csv"
 
 
 quality_scores = {}
@@ -34,6 +37,10 @@ with open(quality_file) as fileObject:
     for row in reader:
         imagePath = row.pop(0)
         image_name = imagePath.split('/')[-1]
+
+        # ==== TEMPORARY ADD "aligned_" ====
+        image_name = "aligned_" + image_name
+
         # quality_score = int(row.pop(28)) # For UQS scalar from OFIQ output file
         quality_score = int(float(row.pop(0))) # For predicted UQS by random forest model
         quality_scores[image_name] = quality_score
@@ -46,6 +53,8 @@ score_pairs_for_EDC = []
 for i in range(0, len(similarity_scores)):
     img1 = similarity_scores[i][0]
     img2 = similarity_scores[i][1]
+    if img1 not in quality_scores or img2 not in quality_scores: # TODO: we should probably fix this before running this script
+        continue
     quality_score1 = quality_scores[img1]
     quality_score2 = quality_scores[img2]
     qs = min(quality_score1, quality_score2) # use lowest quality score for EDC. De facto standard.
@@ -54,7 +63,8 @@ for i in range(0, len(similarity_scores)):
 print(len(score_pairs_for_EDC))
 
 
-output_file = "./score_pairs_for_EDC_Facenet512_03_Yunet_01_and_LFW-trained_Scalar-extra-removed_Predicted-UQS_LFW.csv"
+# output_file = "./score_pairs_for_EDC_Facenet512_03_Yunet_01_and_LFW-trained_Scalar-extra-removed_Predicted-UQS_LFW.csv"
+output_file = "./dissimilarity_and_quality_score_pairs/score_pairs_for_EDC_Facenet512_03_Yunet_01_and_Predicted_UQS_Scalar_to_Scalar_VGGFace200k-n431-502.csv"
 with open(output_file, 'w', newline='') as output_csv:
     writer = csv.writer(output_csv, delimiter=',')
     for i in range(0, len(score_pairs_for_EDC)):
@@ -62,5 +72,6 @@ with open(output_file, 'w', newline='') as output_csv:
 
 
 
+print(f"Done. Saved pairs to {output_file}")
 
 
